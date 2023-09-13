89d120fbb0e3f261c8017a5fe8d2a8610ae2091f
ohmina <orange9751@naver.com>
2023-09-13 15:59:06 +0900

    도형 이미지 커밋 완료

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <div className="shape square01"></div>
+        <div className="shape square02"></div>
+        <div className="shape square03"></div>
+        <div className="shape square04"></div>
+        <div className="shape square05"></div>
+        <div className="shape square06"></div>
+        <div className="shape square07"></div>
+        <div className="shape square08"></div>
+        <div className="shape square09"></div>
+        <div className="shape square10"></div>
+        <div className="shape square11"></div>
+    </div>
+  );
+}
+
+export default App;
